dc4fad84117be4d4952a0189b4402a9dc6729484
Chenyuxin <2636492078@qq.com>
2024-05-19 18:59:17 +0800

    test代码

---
+++ src/test/java/com/creatorsn/fabulous/MyTest.java
@@ -0,0 +1,13 @@
+package com.creatorsn.fabulous;
+
+import net.minidev.json.JSONUtil;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MyTest {
+    @Test
+    public void test(){
+        System.out.println("hello world");
+    }
+}
